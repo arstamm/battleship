80273fa9287b7abedcffd67eb0ace5b6f2694eb1
Andrew <84246113+arstamm@users.noreply.github.com>
2025-03-07 11:39:05 -0700

    Revert "Added default gezz display"
    This reverts commit 2a180de7e08e102aeedc0952998d5396992f541d.

--- battleship_with_cargo/src/main.rs
+++ battleship_with_cargo/src/main.rs
@@ -5,51 +5,7 @@
 
 use std::io;
 
-use ggez::event;
-use ggez::graphics::{self, Color};
-use ggez::{Context, GameResult};
-use ggez::glam::*;
-
-struct MainState {
-    pos_x: f32,
-}
-
-impl MainState {
-    fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
-        Ok(s)
-    }
-}
-
-impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(
-            ctx,
-            graphics::Color::from([0.0, 0.0, 1.0, 1.0]),
-        );
-
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            100.0,
-            2.0,
-            Color::RED,
-        )?;
-        canvas.draw(&circle, Vec2::new(self.pos_x, 380.0));
-
-        canvas.finish(ctx)?;
-        Ok(())
-    }
-}
-
-pub fn main() -> GameResult {
-
+fn main() {
     let mut grid: [[i32; 10]; 10] = [[0; 10]; 10];
 
     // Print the initial grid
@@ -63,11 +19,6 @@ pub fn main() -> GameResult {
     grid[row][col] = value;
     println!("\nUpdated Grid:");
     print_grid(&grid);
-
-    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
-    let (ctx, event_loop) = cb.build()?;
-    let state = MainState::new()?;
-    event::run(ctx, event_loop, state)
 }
 
 // Function to print the grid
@@ -115,4 +66,3 @@ fn get_user_input() -> (usize, usize, i32) {
 
     (row, col - 1, value)
 }
-
