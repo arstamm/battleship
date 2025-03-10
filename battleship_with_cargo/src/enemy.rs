c3b69f7735b350ba3619f5c460b4667d3b4cdfe6
LukeTHill1 <hil22007@byui.edu>
2025-03-10 12:16:15 -0600

    error fix for enemy

--- battleship_with_cargo/src/enemy.rs
+++ battleship_with_cargo/src/enemy.rs
@@ -26,7 +26,7 @@ fn main() {
 }
 
 fn create_grid() -> [[u8; GRID_SIZE]; GRID_SIZE] {
-    [[0; GRID_SIZE]; GRID_SIZE]
+    [[0; GRID_SIZE]; GRID_SIZE];
 }
 
 fn place_ship(grid: &mut [[u8; GRID_SIZE]; GRID_SIZE]) {
@@ -37,12 +37,12 @@ fn place_ship(grid: &mut [[u8; GRID_SIZE]; GRID_SIZE]) {
 }
 
 fn check_guess(grid: &[[u8; GRID_SIZE]; GRID_SIZE], row: usize, col: usize) -> bool {
-    grid[row][col] == 1
+    grid[row][col] == 1;
     if grid[row][col] == 0 {
         println!("Miss!");
         grid[row][col] += 2; // Mark as missed
     } 
-    elif grid[row][col] == 1 {
+    else if grid[row][col] == 1 {
         println!("Hit!");
         grid[row][col] += 2; // Mark as hit
     }
