5697e48a15cc27db634f982ee7277d1550159495
Andrew <84246113+arstamm@users.noreply.github.com>
2025-03-27 22:28:31 -0600

    Matched colors with sprites

--- battleship_with_cargo/src/gameplay/constants.rs
+++ battleship_with_cargo/src/gameplay/constants.rs
@@ -21,7 +21,11 @@ const SILVER: Color = Color::new(0.8078431, 0.8078431, 0.8078431, 1.0);
 const RAISIN_BLACK: Color = Color::new(0.1529412, 0.1490196, 0.2078431, 1.0);
 
 // 10, 76, 126 Marine Blue
-const MARINE_BLUE: Color = Color::new(0.0392157, 0.2980392, 0.4941176, 1.0);
+// const MARINE_BLUE: Color = Color::new(0.0392157, 0.2980392, 0.4941176, 1.0);
+
+// 19, 159, 236 Blue
+const BLUE: Color = Color::new(0.0745098, 0.6235294, 0.9254902, 1.0);
+
 
 // 20, 49, 9 Pakistan Green
 const PAKISTAN_GREEN: Color = Color::new(0.0784314, 0.1921569, 0.0352941, 1.0);
@@ -33,7 +37,7 @@ pub const TITLE_BAR: &str = "Battleship";
 // Window Set Up
 
 const SCREEN_SCALE: f32 = 1.0;
-pub const BACKGROUND_COLOR: Color = GHOST_WHITE;
+pub const BACKGROUND_COLOR: Color = RAISIN_BLACK;
 
 pub const GAME_WINDOW: WindowMode = WindowMode {
     width: 2240.0 * SCREEN_SCALE, //2240
@@ -53,8 +57,8 @@ pub const GAME_WINDOW: WindowMode = WindowMode {
 };
 
 // Grid
-pub const GRID_CELL_COLOR: Color = MARINE_BLUE;
-pub const GRID_LINE_COLOR: Color = RAISIN_BLACK;
+pub const GRID_CELL_COLOR: Color = BLUE;
+pub const GRID_LINE_COLOR: Color = Color::BLACK;
 pub const SHIP_COLOR: Color = PAKISTAN_GREEN;
 pub const X_DELTA: f32 = 80.0 * SCREEN_SCALE;
 pub const Y_DELTA: f32 = 80.0 * SCREEN_SCALE;
@@ -71,7 +75,7 @@ pub const FONT_COLOR: Color = RAISIN_BLACK;
 
 // Banner
 
-pub const BANNER_BACKGROUND_COLOR: Color = FRENCH_GRAY;
+pub const BANNER_BACKGROUND_COLOR: Color = SILVER;
 pub const BANNER_X_POS: f32 = X_DELTA;
 pub const BANNER_Y_POS: f32 = 960.0 * SCREEN_SCALE;
 pub const BANNER_WIDTH: f32 = 1700.0 * SCREEN_SCALE;
@@ -79,7 +83,7 @@ pub const BANNER_HEIGHT: f32 = 130.0 * SCREEN_SCALE;
 
 // Button
 
-pub const BUTTON_BACKGROUND_COLOR: Color = SILVER;
+pub const BUTTON_BACKGROUND_COLOR: Color = FRENCH_GRAY;
 pub const BUTTON_X_POS: f32 = 1860.0 * SCREEN_SCALE;
 pub const BUTTON_Y_POS: f32 = 240.0 * SCREEN_SCALE;
 pub const BUTTON_WIDTH: f32 = 300.0 * SCREEN_SCALE;
